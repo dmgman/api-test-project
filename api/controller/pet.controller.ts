44dd097af5f5374a440b44d917f2f34550208a55
dmgman <meflt88@gmail.com>
2021-09-28 00:52:58 +0300

    add new tests

---
+++ api/controller/pet.controller.ts
@@ -0,0 +1,23 @@
+import got from "got";
+import { URLSearchParams } from "url";
+
+export class PetController {
+  async getById(id: number | string) {
+    const res = await got(`http://localhost/v2/pet/${id}`);
+    return JSON.parse(res.body);
+  }
+
+  async findByStatus(status: string | string[]) {
+    const res = await got(`http://localhost/v2/pet/findByStatus`, {
+      searchParams: new URLSearchParams({ status }),
+    });
+    return JSON.parse(res.body);
+  }
+
+  async findByTags(tags: string | string[]) {
+    const res = await got(`http://localhost/v2/pet/findByTags`, {
+      searchParams: new URLSearchParams({ tags }),
+    });
+    return JSON.parse(res.body);
+  }
+}
--- test/pet.test.ts
+++ test/pet.test.ts
@@ -1,20 +1,42 @@
-import got  from 'got';
-import { strict as assert } from 'assert';
-import console from 'console';
+import { strict as assert } from "assert";
+import { PetController } from "../api/controller/pet.controller";
+import console, { Console } from "console";
+
+const pet = new PetController();
 
 describe("User can", () => {
-  it('can get pet by id', async function () {
-    const response = await got('https://petstore.swagger.io/v2/pet/1');
-    const body = JSON.parse(response.body);
-    console.log(body);
+  it("get pet by id", async function () {
+    const response = await pet.getById(1);
+    assert(response.id == 1, `Error found: ${response.id}, not: 1`);
+  });
 
-    assert(body.id == 1);
-    })
-  it('smth', async function () {
-    const response = await got('http://localhost:3000/profile');
-    const body = JSON.parse(response.body);
-    console.log(body);
+  it("recive by status", async function () {
+    let res = await pet.findByStatus("available");
+    assert(res.length > 0);
 
-    assert(body.name == 'typicode');
-  })
+    res = await pet.findByStatus("sold");
+    assert(res.length > 0);
+
+    res = await pet.findByStatus("pending");
+    assert(res.length > 0);
+
+    res = await pet.findByStatus(["pending", "available"]);
+
+    assert(res.length > 0);
+    assert(res.some((pet: any) => pet.status == "available"));
+    assert(res.some((pet: any) => pet.status == "pending"));
+    assert(!res.some((pet: any) => pet.status == "sold"));
+  });
+
+  it("recive by tag", async function () {
+    const res = await pet.findByTags("tag1");
+    console.log(res)
+
+    assert(res.lenght > 0);
+    assert(
+      res.every(
+        (pet: any) => pet.tags.some(
+        (tag: any) => tag.name == "tag1"))
+    );
+  });
 });
